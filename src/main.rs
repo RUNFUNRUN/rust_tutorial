6345e21f12581489b04f23b53eaa5c88bb8c2740
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-18 18:17:46 +0900

    一旦休憩

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-mod ownership;
+// mod ownership;
 // mod stack_heap;
 // mod vars;
+// mod generics;
+// mod lifetime;
+// mod structs;
+// mod enums;
+// mod traits;
+mod error_handling;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
     // stack_heap::run();
-    ownership::run();
+    // ownership::run();
+    // generics::run();
+    // lifetime::run();
+    // structs::run();
+    // enums::run();
+    // traits::run();
+    error_handling::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
